41ae76a9951904e55525c538e2adf8c9834b104b
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2022-06-13 15:52:43 +0300

    Proje Oluşturuldu

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BuradaEvsizVar());
+}
+
+class BuradaEvsizVar extends StatelessWidget {
+  const BuradaEvsizVar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Burada Evsiz Var',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: ,
+    );
+  }
+}
